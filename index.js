b6bd5be9ca2d6717f64da4fd1f33b1bd1bdcae98
chenyating <924471303@qq.com>
2018-08-31 20:22:05 +0800

    优化界面

--- index.js
+++ index.js
@@ -7,93 +7,118 @@ var app = new Vue({
     foodX: null,
     foodY: null,
     context: null,
-    one: 0,
-    snaker: []
+    gameState: 0,
+    snaker: [],
+    v:300,
   },
   methods: {
     //随机生成x坐标
     radomx() {
-      this.radomX = parseInt(Math.random() * 13);
+      this.radomX = parseInt(Math.random() * 39);
       return this.radomX;
     },
     //随机生成y坐标
     radomy() {
-      this.radomY = parseInt(Math.random() * 25);
+      this.radomY = parseInt(Math.random() * 75);
       return this.radomY;
     },
-    //随机生成头的坐标
-    addSnaker() {
+    //随机生成蛇头的坐标
+    snakerHead() {
+      //清空蛇身
       this.snaker.splice(0, this.snaker.length);
       this.snaker.push([this.radomx() * 20, this.radomy() * 20]);
-      console.log(this.snaker);
+      //蛇头也不能和食物重复；
+        if (this.foodX == this.snaker[0][0] && this.foodY == this.snaker[0][1]) {
+          return this.snakerHead();
+        }
     },
-    //显示食物的坐标红色，且不能和蛇头在同一个位置。
+    //随机生成食物的坐标，且不能和蛇身，蛇头重合
     food() {
       this.foodX = this.radomx() * 20;
       this.foodY = this.radomy() * 20;
-      //判断食物不能和头重复了。
+      //判断食物不能和蛇重复了。
       for (var i = 0; i < this.snaker.length; i++) {
+        //只要出现重复，则重新调用food（）；
         if (this.foodX == this.snaker[i][0] && this.foodY == this.snaker[i][1]) {
           return this.food();
         }
       }
+      this.painFood();
+    },
+    //绘制红色食物
+    painFood() {
       var c = document.getElementById("tanchi");
       this.context = c.getContext("2d");
-      this.context.fillStyle = "#FE4C40";
+      this.context.fillStyle = "#8A3324";
       this.context.fillRect(this.foodX, this.foodY, 20, 20);
     },
+    //是否出界
     ifOut() {
       if (
         0 > this.snaker[0][0] ||
         this.snaker[0][0] > 780 ||
-        980 < this.snaker[0][1] ||
+        1480 < this.snaker[0][1] ||
         this.snaker[0][1] < 0
       ) {
-        alert("游戏结束,你跳出方框了！");
-        this.snaker.splice(0, this.snaker.length);
-        this.one = 0;
-        this.scope = 0;
-        //清空整个画布
-        this.context.clearRect(0, 0, 800, 1000);
-        $("#title").html("");
-        $("#title").html("游戏开始");
+        alert("游戏结束,你撞到墙了");
+        this.gameOver();
+      }
+      for (var i = this.snaker.length - 1; i > 0; i--) {
+        if (this.snaker[0][0] == this.snaker[i][0] && this.snaker[0][1] == this.snaker[i][1]) {
+          alert("游戏结束，你撞到你自己啦！")
+          this.gameOver();
+        }
       }
     },
+    //游戏结束
+    gameOver() {
+      //清空蛇身；
+      this.snaker.splice(0, this.snaker.length);
+      //游戏分值清空，游戏状态改为0；
+      this.gameState = 0;
+      this.scope = 0;
+      //清空整个画布
+      this.context.clearRect(0, 0, 800, 1500);
+      $("#title").html("");
+      $("#title").html("游戏开始");
+    },
+    //游戏开始
     begin() {
-      if (this.one != 0) {
+      if (this.gameState != 0) {
+        //游戏结束状态
         $("#title").html("");
         $("#title").html("游戏开始");
-        this.one = 0;
+        this.gameState = 0;
         this.scope = 0;
         //清空整个画布
-        this.context.clearRect(0, 0, 800, 1000);
+        this.context.clearRect(0, 0, 800, 1500);
       } else {
-        this.addSnaker();
+        //游戏开始状态
         this.food();
-        this.context.fillStyle = "#00FF80";
-        this.context.fillRect(this.snaker[0][0], this.snaker[0][1], 20, 20);
-        this.one = 1;
+        this.snakerHead();
+        this.painSnakerHead()
+        this.gameState = 1;
         $("#title").html("");
         $("#title").html("游戏结束");
-        //绘制食物
       }
     },
+    //蛇在x轴的变化
     changgeX(x) {
       this.context.clearRect(this.snaker[this.snaker.length - 1][0], this.snaker[this.snaker.length - 1][1], 20, 20);
       for (var i = this.snaker.length - 1; i >= 0; i--) {
         if (i == 0) {
           this.snaker[0][0] = this.snaker[0][0] + x;
+          this.painSnakerHead();
         } else {
           this.snaker[i][0] = this.snaker[i - 1][0];
           this.snaker[i][1] = this.snaker[i - 1][1];
+          this.painSnakerBody();
+          this.context.fillRect(this.snaker[i][0], this.snaker[i][1], 20, 20);
         }
-        this.context.fillRect(this.snaker[i][0], this.snaker[i][1], 20, 20);
-        this.painSnakerBody();
       }
+      //食物出现的位置和蛇头重合，则在末尾加上食物的位置，就是y轴最后+-20；
       if (this.foodX == this.snaker[0][0] && this.foodY == this.snaker[0][1]) {
-        console.log(this.foodX + "+" + this.foodY);
         this.snaker.push([this.foodX - x, this.foodY]);
-        console.log(this.snaker);
         this.context.fillRect(
           this.snaker[this.snaker.length - 1][0],
           this.snaker[this.snaker.length - 1][1],
@@ -104,24 +129,25 @@ var app = new Vue({
         this.food();
         this.scope = this.scope + 1;
       }
-      console.log('changge' + this.snaker)
+      this.ifOut();
     },
+    //蛇在y轴的变化
     changgeY(y) {
       this.context.clearRect(this.snaker[this.snaker.length - 1][0], this.snaker[this.snaker.length - 1][1], 20, 20);
       for (var i = this.snaker.length - 1; i >= 0; i--) {
         if (i == 0) {
           this.snaker[0][1] = this.snaker[0][1] - y;
+          this.painSnakerHead();
         } else {
           this.snaker[i][0] = this.snaker[i - 1][0];
           this.snaker[i][1] = this.snaker[i - 1][1];
+          this.painSnakerBody();
+          this.context.fillRect(this.snaker[i][0], this.snaker[i][1], 20, 20);
         }
-        this.context.fillRect(this.snaker[i][0], this.snaker[i][1], 20, 20);
-        this.painSnakerBody();
       }
+      //食物出现的位置和蛇头重合，则在末尾加上食物的位置，就是y轴最后+-20；
       if (this.foodX == this.snaker[0][0] && this.foodY == this.snaker[0][1]) {
-        console.log(this.foodX + "+" + this.foodY);
         this.snaker.push([this.foodX, this.foodY + y]);
-        console.log(this.snaker);
         this.context.fillRect(
           this.snaker[this.snaker.length - 1][0],
           this.snaker[this.snaker.length - 1][1],
@@ -132,14 +158,20 @@ var app = new Vue({
         this.food();
         this.scope = this.scope + 1;
       }
-      console.log('changge' + this.snaker)
-    },
-    painSnakerBody() {
-      this.context.fillStyle = "#00FF80";
       this.ifOut();
-    }
+    },
+    //画蛇身
+    painSnakerBody() {
+      this.context.fillStyle = "#363636";
+    },
+    //画蛇头
+    painSnakerHead() {
+      this.context.fillStyle = "#00755E";
+      this.context.fillRect(this.snaker[0][0], this.snaker[0][1], 20, 20);
+    },
   },
-  mounted() {},
+  mounted() {
+  },
   watch: {}
 });
 var startx, starty;
@@ -172,6 +204,8 @@ function getDirection(startx, starty, endx, endy) {
     result = 3;
   } else if (angle >= -45 && angle <= 45) {
     result = 4;
+  } else {
+    result = 0;
   }
 
   return result;
@@ -188,10 +222,16 @@ document.addEventListener(
 
 //手指离开屏幕
 let timer = null;
+//速度控制
+app.v = 300;
+if (app.scope >= 10) {
+  var j = parseInt(app.scope / 10);
+  app.v = 300 - 20 * j;
+}
 document.addEventListener(
   "touchend",
   function (e) {
-    if (app.one == 0) {
+    if (app.gameState == 0) {
       return;
     }
     var endx, endy;
@@ -202,22 +242,25 @@ document.addEventListener(
     if (direction == 1) {
       timer = setInterval(() => {
         app.changgeY(20);
-      }, 500)
+      }, app.v)
     }
     if (direction == 2) {
       timer = setInterval(() => {
         app.changgeY(-20);
-      }, 500)
+      }, app.v)
     }
     if (direction == 3) {
       timer = setInterval(() => {
         app.changgeX(-20);
-      }, 500)
+      }, app.v)
     }
     if (direction == 4) {
       timer = setInterval(() => {
         app.changgeX(20);
-      }, 500)
+      }, app.v)
+    }
+    if (direction == 0) {
+      clearInterval(timer);
     }
   },
   false
