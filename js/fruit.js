8733fcbdcca607a1a6d4100d0815b427e8ff772e
chenyating <924471303@qq.com>
2018-12-08 16:47:53 +0800

    下午茶功能完善

--- js/fruit.js
+++ js/fruit.js
@@ -35,12 +35,19 @@ var fruit = new Vue({
     },
     methods: {
         beginFruit() {
+            this.otext=null;
             this.bigFruit();
             this.middleFruit();
             this.smallFruit();
             this.totalMoney();
         },
         bigFruit() {
+            if(this.bnum==0){
+                this.btext="0元";
+                this.bprice=0;
+                this.bg=0;
+                return;
+            }
             var id = parseInt(Math.random() * this.blist.length);
             // 大型水果价格约:一个大型水果约重*价格*个数
             this.bprice = this.blist[id].weight * this.blist[id].price * this.bnum;
@@ -48,6 +55,12 @@ var fruit = new Vue({
             this.bg = this.blist[id].weight * this.bnum;
         },
         middleFruit() {
+            if(this.mnum==0){
+                this.mtext="0元";
+                this.mprice=0;
+                this.mg=0;
+                return;
+            }
             var id = parseInt(Math.random() * this.mlist.length);
             // 中型水果价格约:（中型水果个数/2）*价格
             this.mprice = (this.mnum / 2) * this.mlist[id].price;
@@ -55,6 +68,12 @@ var fruit = new Vue({
             this.mg = this.mnum / 2;
         },
         smallFruit() {
+            if(this.snum==0){
+                this.stext="0元";
+                this.sprice=0;
+                this.sg=0;
+                return;
+            }
             var id = parseInt(Math.random() * this.slist.length);
             // 小型水果价格约:小型水果斤数*价格
             this.sprice = this.slist[id].price * this.snum;
@@ -62,29 +81,46 @@ var fruit = new Vue({
             this.sg = this.snum;
         },
         otherSmall() {
-            this.og=0;
-            var lastprice = parseInt(this.budget - this.tprice);
-            var id = parseInt(Math.random() * this.slist.length);
-            // 没有超出预算还能购买小型水果是::（预算减去约价格）\小型水果价格
-            this.og = parseInt(lastprice / this.slist[id].price);
-            if (this.og <= 0) {
-                this.otherSmall();
+            if( this.ttext == "超出预算了"){
+                this.otext = "不能买其他小型水果了";
                 return;
             }
             else{
-                this.otext = "不到预算可以再买约" + this.og + "斤的" + this.slist[id].name + ",价格为:" + this.slist[id].price + "元/斤,总计约:" + this.slist[id].price * this.og + "元";
+                this.og = 0;
+                this.otext = "不能买其他小型水果了";
+                var lastprice = this.budget - this.tprice;
+                var id = parseInt(Math.random() * this.slist.length);
+                // 没有超出预算还能购买小型水果是::（预算减去约价格）\小型水果价格
+                this.og = parseInt(lastprice / this.slist[id].price);
+                try {
+                    if (this.og <= 1) {
+                        this.otext = "不能买其他小型水果了";
+                        this.otherSmall();
+                        return;
+                        // 因为一直在随机，没有得出结果来，就一直在随机。会报错
+                    } else {
+                        this.otext = "可再买：约" + this.og + "斤的" + this.slist[id].name + ",价格为:" + this.slist[id].price + "元/斤,总计约:" + this.slist[id].price * this.og + "元";
+                    }
+                } catch (error) {}
             }
         },
         totalMoney() {
             this.tprice = this.bprice + this.mprice + this.sprice;
-            if (this.tprice >= this.budget) {
-                this.beginFruit();
-                return;
-            } else {
-                this.tg = this.bg + this.mg + this.snum;
-                this.ttext = "总价格约:" + this.tprice + "元,约:" + this.tg + "斤";
-                this.otherSmall();
+            try {
+                if (this.tprice >= this.budget) {
+                    this.ttext = "超出预算了";
+                    this.tprice=null;
+                    this.beginFruit();
+                    return;
+                } else {
+                    this.tg = this.bg + this.mg + this.snum;
+                    this.ttext = this.tprice + "元,约:" + this.tg + "斤";
+                    this.otherSmall();
+                }
+            } catch (error) {
+
             }
+
         }
     },
     mounted() {
